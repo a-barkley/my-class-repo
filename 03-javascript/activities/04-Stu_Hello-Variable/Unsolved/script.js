e3ea1c0f19b124b22642b3c805efbf320f7a7b43
pbarkley <42686983+pbarkley@users.noreply.github.com>
2022-06-22 11:52:37 -0500

    class update 06-22

---
+++ 03-javascript/activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -0,0 +1,9 @@
+// Write Your JavaScript Code Here
+var myName="Pat";
+var pets=2;
+var funFact="this is a fun fact";
+
+console.log("My name is " + myName + ".")
+console.log("I have " + pets + " pets.")
+console.log("Fun fact: " + funFact + ".")
+console.log(myName + " " + pets)
